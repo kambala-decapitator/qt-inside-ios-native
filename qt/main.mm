3990100ec384368be5c1f0cb43d1f43ada819454
Andrey Filipenkov <decapitator@ukr.net>
2024-05-20 14:06:27 +0300

    fix embedding with Qt >= 6.5

--- qt/main.mm
+++ qt/main.mm
@@ -3,18 +3,33 @@
 #include <QGuiApplication>
 #include <QWindow>
 
+namespace
+{
+UIView* addQtWindowNativeViewToNativeWindow(UIWindow* window)
+{
+	auto qtWindow = qApp->topLevelWindows()[0];
+	auto qtWindowNativeView = (__bridge UIView*)reinterpret_cast<void*>(qtWindow->winId());
+	[window.rootViewController.view addSubview:qtWindowNativeView];
+	return qtWindowNativeView;
+}
+}
+
 void showQtWindow()
 {
 	auto app = UIApplication.sharedApplication;
+
+	// TODO: checked if needed in 6.4
+#if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
+	auto nativeView = addQtWindowNativeViewToNativeWindow(app.keyWindow);
+	nativeView.frame = nativeView.superview.bounds;
+#else
 	auto qtNativeWindowIndex = [app.windows indexOfObjectPassingTest:^BOOL(__kindof UIWindow * _Nonnull window, NSUInteger idx, BOOL * _Nonnull stop) {
 		return [NSStringFromClass([window class]) isEqualToString:@"QUIWindow"];
 	}];
 	Q_ASSERT(qtNativeWindowIndex != NSNotFound);
 
-	auto qtWindow = qApp->topLevelWindows()[0];
-	auto qtWindowNativeView = (__bridge UIView*)reinterpret_cast<void*>(qtWindow->winId());
-
 	auto qtNativeWindow = app.windows[qtNativeWindowIndex];
-	[qtNativeWindow.rootViewController.view addSubview:qtWindowNativeView];
+	addQtWindowNativeViewToNativeWindow(qtNativeWindow);
 	[qtNativeWindow makeKeyAndVisible];
+#endif
 }
